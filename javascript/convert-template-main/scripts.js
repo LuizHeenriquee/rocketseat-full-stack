69b9458170240611aff52de58eb125e5289d17e7
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-11-28 19:57:44 -0300

    Exibindo o footer e manipulando as informações através do Js

--- javascript/convert-template-main/scripts.js
+++ javascript/convert-template-main/scripts.js
@@ -7,6 +7,8 @@ const GBP = 7.53;
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
@@ -33,5 +35,12 @@ form.onsubmit = (event) => {
 
 // Função para converter a moeda
 const convertCurrency = (amount, price, symbol) => {
-  console.log(amount, price, symbol);
+  try {
+    description.textContent = `${symbol}1 = R$${price}`;
+    footer.classList.add("show-result");
+  } catch (error) {
+    footer.classList.remove("show-result");
+    console.log(error);
+    alert("Não foi possível converter.");
+  }
 };
