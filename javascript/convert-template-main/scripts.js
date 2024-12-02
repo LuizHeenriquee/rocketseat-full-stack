c31d62abc2f302b1804afe26406b11ff8015f0e1
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-12-02 16:50:38 -0300

    Formatando o valor total.

--- javascript/convert-template-main/scripts.js
+++ javascript/convert-template-main/scripts.js
@@ -9,6 +9,7 @@ const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
 const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
@@ -37,6 +38,11 @@ form.onsubmit = (event) => {
 const convertCurrency = (amount, price, symbol) => {
   try {
     description.textContent = `${symbol}1 = ${formatCurrencyBRL(price)}`;
+
+    let total = amount * price;
+    total = formatCurrencyBRL(total).replace("R$", "");
+    result.textContent = `R$${total}`;
+
     footer.classList.add("show-result");
   } catch (error) {
     footer.classList.remove("show-result");
